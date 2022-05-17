fe7108edbf6034008c4057dfbfcc879e61b82d28
Bui Tan Phat <phatbt@incognito.org>
2022-05-17 14:36:58 +0700

    Add API folder

---
+++ app/services/api/masterKey.ts
@@ -0,0 +1,32 @@
+import http from "@services/http";
+
+export const getWalletAccounts = async (masterAccountPublicKey: any) => {
+  let result = [];
+  try {
+    const url = `hd-wallet/recovery?Key=${masterAccountPublicKey}`;
+    const res: any = await http.get(url);
+    result =
+      res?.Accounts?.map((account: any) => ({
+        name: account?.Name,
+        id: account?.AccountID,
+      })) || [];
+  } catch {
+    //
+  }
+  return result;
+};
+
+export const updateWalletAccounts = (masterAccountPublicKey: any, accounts: any) => {
+  const accountInfos = accounts.map((item: any) => ({
+    Name: item.name,
+    AccountID: item.id,
+  }));
+  return http
+    .put("hd-wallet/recovery", {
+      Key: masterAccountPublicKey,
+      Accounts: accountInfos,
+    })
+    .catch((e: any) => {
+      console.log("updateWalletAccounts ERROR ", e);
+    });
+};
