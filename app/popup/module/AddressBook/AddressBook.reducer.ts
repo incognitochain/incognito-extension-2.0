f7af8882274e98974ee43e69850969aa58f18f0c
SangNX <sang@incognito.org>
2022-06-09 10:44:02 +0700

    [Webext] send form

--- app/popup/module/AddressBook/AddressBook.reducer.ts
+++ app/popup/module/AddressBook/AddressBook.reducer.ts
@@ -1,86 +1,84 @@
-// import { persistReducer } from 'redux-persist';
-// import autoMergeLevel2 from 'redux-persist/es/stateReconciler/autoMergeLevel2';
-// import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
-// import { isKeyChainExist } from 'src/utils/keychain';
-// import {
-//     ACTION_CREATE,
-//     ACTION_DELETE,
-//     ACTION_UPDATE,
-//     ACTION_SELECTED,
-//     ACTION_REMOVE_SELECTED,
-//     ADDRESS_BOOK_TYPE,
-// } from './AddressBook.constant';
-// import { IAddressBook, IAddressBookReducer, IPayload } from './AddressBook.interface';
-//
-// const initialState: IAddressBookReducer = {
-//     incognitoAddress: [],
-//     externalAddress: [],
-//     selected: undefined,
-// };
-//
-// const addressBookReducer = (state = initialState, action: { type: string; payload: any }) => {
-//     switch (action.type) {
-//         case ACTION_CREATE: {
-//             const { addressBook }: IPayload = action.payload;
-//             const field = ADDRESS_BOOK_TYPE[addressBook.type];
-//             const oldAddressedBook: IAddressBook[] = state[field];
-//             return {
-//                 ...state,
-//                 [field]: [...oldAddressedBook, { ...addressBook, createdAt: new Date().getTime() }],
-//             };
-//         }
-//         case ACTION_UPDATE: {
-//             const { addressBook }: IPayload = action.payload;
-//             const field = ADDRESS_BOOK_TYPE[addressBook.type];
-//             const oldAddressedBook: IAddressBook[] = state[field];
-//             return {
-//                 ...state,
-//                 [field]: [
-//                     ...oldAddressedBook.map((item) =>
-//                         item.address === addressBook.address
-//                             ? { ...item, ...addressBook, updatedAt: new Date().getTime() }
-//                             : item,
-//                     ),
-//                 ],
-//             };
-//         }
-//         case ACTION_DELETE: {
-//             const { addressBook }: IPayload = action.payload;
-//             const field = ADDRESS_BOOK_TYPE[addressBook.type];
-//             const oldAddressedBook: IAddressBook[] = state[field];
-//             const isExist = isKeyChainExist(oldAddressedBook, addressBook);
-//             if (!isExist) {
-//                 return state;
-//             }
-//             return {
-//                 ...state,
-//                 [field]: [...oldAddressedBook.filter((item) => item.address !== addressBook.address)],
-//             };
-//         }
-//         case ACTION_SELECTED: {
-//             const { addressBook }: { addressBook: IAddressBook } = action.payload;
-//             return {
-//                 ...state,
-//                 selected: { ...addressBook },
-//             };
-//         }
-//         case ACTION_REMOVE_SELECTED: {
-//             return {
-//                 ...state,
-//                 selected: null,
-//             };
-//         }
-//         default:
-//             return state;
-//     }
-// };
-//
-// const persistConfig = {
-//     key: 'addressBook',
-//     storage,
-//     whitelist: ['incognitoAddress', 'externalAddress'],
-//     stateReconciler: autoMergeLevel2,
-// };
-//
-// export default persistReducer(persistConfig, addressBookReducer);
-export default {};
+import { persistReducer } from "redux-persist";
+import autoMergeLevel2 from "redux-persist/es/stateReconciler/autoMergeLevel2";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+// import { isKeyChainExist } from "src/utils/keychain";
+import {
+  ACTION_CREATE,
+  ACTION_DELETE,
+  ACTION_UPDATE,
+  ACTION_SELECTED,
+  ACTION_REMOVE_SELECTED,
+  ADDRESS_BOOK_TYPE,
+} from "./AddressBook.constant";
+import { IAddressBook, IAddressBookReducer, IPayload } from "./AddressBook.interface";
+
+const initialState: IAddressBookReducer = {
+  incognitoAddress: [],
+  externalAddress: [],
+  selected: undefined,
+};
+
+const addressBookReducer = (state = initialState, action: { type: string; payload: any }) => {
+  switch (action.type) {
+    case ACTION_CREATE: {
+      const { addressBook }: IPayload = action.payload;
+      const field = ADDRESS_BOOK_TYPE[addressBook.type];
+      const oldAddressedBook: IAddressBook[] = state[field];
+      return {
+        ...state,
+        [field]: [...oldAddressedBook, { ...addressBook, createdAt: new Date().getTime() }],
+      };
+    }
+    case ACTION_UPDATE: {
+      const { addressBook }: IPayload = action.payload;
+      const field = ADDRESS_BOOK_TYPE[addressBook.type];
+      const oldAddressedBook: IAddressBook[] = state[field];
+      return {
+        ...state,
+        [field]: [
+          ...oldAddressedBook.map((item) =>
+            item.address === addressBook.address ? { ...item, ...addressBook, updatedAt: new Date().getTime() } : item,
+          ),
+        ],
+      };
+    }
+    case ACTION_DELETE: {
+      const { addressBook }: IPayload = action.payload;
+      const field = ADDRESS_BOOK_TYPE[addressBook.type];
+      const oldAddressedBook: IAddressBook[] = state[field];
+      // const isExist = isKeyChainExist(oldAddressedBook, addressBook);
+      const isExist = false;
+      if (!isExist) {
+        return state;
+      }
+      return {
+        ...state,
+        [field]: [...oldAddressedBook.filter((item) => item.address !== addressBook.address)],
+      };
+    }
+    case ACTION_SELECTED: {
+      const { addressBook }: { addressBook: IAddressBook } = action.payload;
+      return {
+        ...state,
+        selected: { ...addressBook },
+      };
+    }
+    case ACTION_REMOVE_SELECTED: {
+      return {
+        ...state,
+        selected: null,
+      };
+    }
+    default:
+      return state;
+  }
+};
+
+const persistConfig = {
+  key: "addressBook",
+  storage,
+  whitelist: ["incognitoAddress", "externalAddress"],
+  stateReconciler: autoMergeLevel2,
+};
+
+export default persistReducer(persistConfig, addressBookReducer);
--- app/popup/module/AddressBook/AddressBook.selector.ts
+++ app/popup/module/AddressBook/AddressBook.selector.ts
@@ -1,50 +1,48 @@
-// import { IAddressBook } from 'src/module/AddressBook';
-// import { AccountInstance } from 'incognito-js/build/web/browser';
-// import { createSelector } from 'reselect';
-// import { IRootState } from 'src/redux/interface';
-// import { isMainnetSelector } from 'src/module/Preload/Preload.selector';
-// import { fullListAccountSelector } from 'src/module/HDWallet/HDWallet.selector';
+// import { IAddressBook } from "@module/AddressBook";
+// import { createSelector } from "reselect";
+// import { isMainnetSelector } from "src/module/Preload/Preload.selector";
+// import { fullListAccountSelector } from "src/module/HDWallet/HDWallet.selector";
 //
 // export const addressBookSelector = createSelector(
-//     (state: IRootState) => state.addressBook,
-//     isMainnetSelector,
-//     (addressBook, mainnet) => ({
-//         ...addressBook,
-//         incognitoAddress: addressBook.incognitoAddress.filter((item: IAddressBook) => item.mainnet === mainnet) || [],
-//         externalAddress: addressBook.externalAddress.filter((item: IAddressBook) => item.mainnet === mainnet) || [],
-//     }),
+//   (state: RootState) => state.addressBook,
+//   isMainnetSelector,
+//   (addressBook, mainnet) => ({
+//     ...addressBook,
+//     incognitoAddress: addressBook.incognitoAddress.filter((item: IAddressBook) => item.mainnet === mainnet) || [],
+//     externalAddress: addressBook.externalAddress.filter((item: IAddressBook) => item.mainnet === mainnet) || [],
+//   }),
 // );
 //
 // export const incognitoAddrSelector = createSelector(addressBookSelector, (addressBook) =>
-//     addressBook.incognitoAddress.map((item) => ({ ...item, canBeRemoved: true, canBeEdit: true })),
+//   addressBook.incognitoAddress.map((item: any) => ({ ...item, canBeRemoved: true, canBeEdit: true })),
 // );
 //
 // export const externalAddrSelector = createSelector(addressBookSelector, (addressBook) =>
-//     addressBook.externalAddress.map((item) => ({ ...item, canBeRemoved: true, canBeEdit: true })),
+//   addressBook.externalAddress.map((item: any) => ({ ...item, canBeRemoved: true, canBeEdit: true })),
 // );
 //
-// export const keychainAddrSelector = createSelector(fullListAccountSelector, (accounts) =>
-//     accounts.map((account: AccountInstance) => ({
-//         name: account.name,
-//         address: account.key.keySet.paymentAddressKeySerialized,
-//     })),
+// export const keychainAddrSelector = createSelector(fullListAccountSelector, (accounts: any) =>
+//   accounts.map((account: any) => ({
+//     name: account.name,
+//     address: account.key.keySet.paymentAddressKeySerialized,
+//   })),
 // );
 //
 // export const selectedAddressBookSelector = createSelector(
-//     addressBookSelector,
-//     (addressBookState) => addressBookState.selected,
+//   addressBookSelector,
+//   (addressBookState) => addressBookState.selected,
 // );
 //
 // export const isIncognitoAddressExistSelector = createSelector(
-//     incognitoAddrSelector,
-//     keychainAddrSelector,
-//     (incognitoAddr: IAddressBook[], keychainAddr: { address: string; name: string }[]) => (address: string) =>
-//         incognitoAddr.findIndex((item) => item.address === address) > -1 ||
-//         keychainAddr.findIndex((item) => item.address === address) > -1,
+//   incognitoAddrSelector,
+//   keychainAddrSelector,
+//   (incognitoAddr: IAddressBook[], keychainAddr: { address: string; name: string }[]) => (address: string) =>
+//     incognitoAddr.findIndex((item) => item.address === address) > -1 ||
+//     keychainAddr.findIndex((item) => item.address === address) > -1,
 // );
 //
 // export const isExternalAddressExistSelector = createSelector(
-//     externalAddrSelector,
-//     (externalAddr: IAddressBook[]) => (address: string) => externalAddr.find((item) => item.address === address),
+//   externalAddrSelector,
+//   (externalAddr: IAddressBook[]) => (address: string) => externalAddr.find((item) => item.address === address),
 // );
 export default {};
