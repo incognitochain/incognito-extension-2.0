0895ab3c4a7aa438662a5fd141f44690ddfa57ac
Bui Tan Phat <phatbt@incognito.org>
2022-07-27 14:01:55 +0700

    Modify Manifest file

--- app/background/background.ts
+++ app/background/background.ts
@@ -1,5 +1,6 @@
-const { init } = require("incognito-chain-web-js/build/web/wallet");
-
+// const { init } = require("incognito-chain-web-js/build/web/wallet");
+const test = 1;
 console.log("Background running ... ");
+console.log("TEST ", test);
 
 export default {};
--- app/popup/index.tsx
+++ app/popup/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 console.log("RENDER POPUP");
 ReactDOM.render(
   <React.StrictMode>
-    <></>
+    <div style={{ width: 375, height: 800, backgroundColor: "lightblue" }}></div>
   </React.StrictMode>,
   document.getElementById("root"),
 );
